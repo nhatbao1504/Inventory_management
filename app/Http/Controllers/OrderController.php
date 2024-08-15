0ace2188ec05e37ab1b1c63a6dbcb6128a91ee8f
nhatbao1504 <hnhatbao1504@gmail.com>
2024-08-15 18:23:08 +0700

    them vao OrderModel va them 2 bang du lieu

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     function getView(){
+        $order_project = OrderModel::all();
+
+
+
+
         return view('auth.order.order');
     }
 }
--- app/Models/OrderModel.php
+++ app/Models/OrderModel.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class OrderModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'order_project';
+    protected $primaryKey = 'order_id';
+
+    protected $fillable = [
+        'order_id',
+        'order_code',
+        'order_name',
+        'order_owner',
+        'status',
+        'create_date',
+        'total_price',
+        'VAT'
+    ];
 }
